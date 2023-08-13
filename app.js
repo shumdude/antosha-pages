c33489092e5edee1628b171e9795cd1c9e53cb34
ShumovAleksej <thefid7262@mail.ru>
2023-08-14 00:42:25 +0500

    trying commit

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ let btn = document.getElementById("btn");  // получаем кнопку по
 // слушатель события нажатия кнопки
 btn.addEventListener("click", function () {
     tg.MainButton.setText("bbbb");
-    let data = tg.initData;
     tg.MainButton.show();
     tg.sendData("sendTestMessage");  // отправка в телеграм бота
 });
