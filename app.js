75b0bc4997495ba1829f34e0afd838ff55513234
ShumovAleksej <thefid7262@mail.ru>
2023-08-14 00:41:35 +0500

    trying commit

--- app.js
+++ app.js
@@ -6,12 +6,10 @@ tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#80b5f8";
 
 let btn = document.getElementById("btn");  // получаем кнопку по id bth из html
-let user = document.getElementById("user");
 // слушатель события нажатия кнопки
 btn.addEventListener("click", function () {
     tg.MainButton.setText("bbbb");
     let data = tg.initData;
-    user.textContent = "AAAAA";
     tg.MainButton.show();
     tg.sendData("sendTestMessage");  // отправка в телеграм бота
 });
