623a1e4e3488549c231641ca958597b761980c12
ShumovAleksej <thefid7262@mail.ru>
2023-08-14 00:52:00 +0500

    trying commit

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-let tg = window.Telegram.WebApp;  // окно
+let tg = window.Telegram.WebApp; //получаем объект webapp телеграма
 
-tg.expand();  // расширение окна по высоте экрана
+tg.expand(); //расширяем на все окно
 
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#80b5f8";
+tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
+tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
 
-let btn = document.getElementById("btn");  // получаем кнопку по id bth из html
-// слушатель события нажатия кнопки
-btn.addEventListener("click", function () {
-    tg.MainButton.setText("bbbb");
-    tg.MainButton.show();
-    tg.sendData("sendTestMessage");  // отправка в телеграм бота
+btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+	if (tg.MainButton.isVisible){ //если кнопка показана
+		tg.MainButton.hide() //скрываем кнопку
+	}
+  else{ //иначе
+  	tg.MainButton.show() //показываем
+  }
 });
+
+let btnED = document.getElementById("btnED"); //получаем кнопку активировать/деактивировать
+btnED.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+	if (tg.MainButton.isActive){ //если кнопка показана
+		tg.MainButton.setParams({"color": "#E0FFFF"}); //меняем цвет
+		tg.MainButton.disable() //скрываем кнопку
+	}
+	else{ //иначе
+		tg.MainButton.setParams({"color": "#143F6B"}); //меняем цвет
+		tg.MainButton.enable() //показываем
+	}
+});
+
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+	tg.sendData("some string that we need to send");
+	//при клике на основную кнопку отправляем данные в строковом виде
+});
+
+let usercard = document.getElementById("usercard"); //получаем блок usercard
+
+let profName = document.createElement('p'); //создаем параграф
+profName.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}
+${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
+//выдем имя, "фамилию", через тире username и код языка
+usercard.appendChild(profName); //добавляем
+
+let userid = document.createElement('p'); //создаем еще параграф
+userid.innerText = `${tg.initDataUnsafe.user.id}`; //показываем user_id
+usercard.appendChild(userid); //добавляем
+
