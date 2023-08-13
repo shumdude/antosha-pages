ca52f453e57171f52a2250a372e289c120df0c72
ShumovAleksej <thefid7262@mail.ru>
2023-08-14 00:58:13 +0500

    trying commit

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ tg.MainButton.textColor = "#F55353"; //изменяем цвет текста к
 tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
 tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
 
+let btn = document.getElementById("btn");
 btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
 	if (tg.MainButton.isVisible){ //если кнопка показана
 		tg.MainButton.hide() //скрываем кнопку
