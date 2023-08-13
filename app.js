53aaddd489cf7c8610d0b441a909935f4636b150
ShumovAleksej <thefid7262@mail.ru>
2023-08-13 19:04:28 +0500

    Second commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let tg = window.Telegram.WebApp;  // окно
+
+tg.expand();  // расширение окна по высоте экрана
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#80b5f8";
+
+let btn = document.getElementById("btn");  // получаем кнопку по id bth из html
+
+// слушатель события нажатия кнопки
+btn.addEventListener("click", function () {
+    tg.MainButton.setText("Success!");
+    tg.MainButton.show();
+    tg.sendData("sendTestMessage");  // отправка в телеграм бота
+});
